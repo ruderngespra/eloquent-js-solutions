4e72fe345b0818e9a4b97f7f9c653c1442e5d25c
janko <janko_marklein@riseup.net>
2019-09-23 21:26:51 +0200

    Add first exercise chap12

---
+++ chap12_aProgrammingLanguage/arrays.js
@@ -0,0 +1,47 @@
+const { run, specialForms, topScope, evaluate } = require('./12_language');
+
+// My solutions:
+
+topScope.array = (...arguments) => {
+    let newArray = [];
+    for (let element of arguments) {
+        newArray.push(element);
+    }
+    return newArray;
+};
+
+topScope.length = array => {
+    return array.length;
+};
+
+topScope.element = (array, n) => {
+    return array[n];
+};
+
+// Testing:
+// run(`do(define(x, array(10, 234)),
+// print(x),
+// print(length(x)),
+// print(element(x, 1))
+// )`);
+
+run(`
+do(define(sum, fun(array,
+     do(define(i, 0),
+        define(sum, 0),
+        while(<(i, length(array)),
+          do(define(sum, +(sum, element(array, i))),
+             define(i, +(i, 1)))),
+        sum))),
+   print(sum(array(1, 2, 3))))
+`);
+
+/*
+
+Solution Haverbeke:
+
+topScope.array = (...values) => values;
+topScope.length = array => array.length;
+topScope.element = (array, i) => array[i];
+
+*/
